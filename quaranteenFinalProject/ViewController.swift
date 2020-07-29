aaae4c9539f0b2eb74bd599ac8d97b85a90d78f7
Rachel November <rachelnovember3@gmailcom>
2020-07-29 11:27:34 -0400

    7/29/20, 11:27

--- quaranteenFinalProject/ViewController.swift
+++ quaranteenFinalProject/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func buttonToDo(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonQuizzes(_ sender: UIButton) {
+    }
+    
+    @IBAction func buttonMentalHealth(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
---
+++ quaranteenFinalProject/thirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  thirdViewController.swift
+//  quaranteenFinalProject
+//
+//  Created by Rachel November on 7/29/20.
+//  Copyright © 2020 Rachel November. All rights reserved.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
